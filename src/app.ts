a72f77cabaffb666e0db0e23625ab904798e66fb
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-24 17:58:49 -0300

    feat: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
-export const app = fastify();
+export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Henrique Moreira de Souza',
+    email: 'henriquemoreiradesouza4@gmail.com',
+  },
+})
