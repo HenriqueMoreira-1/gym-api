29966531ff2f300296bb86b15b55583029f5928e
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-26 11:12:44 -0300

    refactor: create controllers to spli my application

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,23 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
