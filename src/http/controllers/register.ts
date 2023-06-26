d8962be2fe71ce2b4d62656caa535093f5aa764c
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-26 12:10:15 -0300

    fe

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { email, name, password } = registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       email,
       name,
-      password_hash: password,
+      password_hash,
     },
   })
 
