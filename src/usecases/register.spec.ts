074f55c3c2db1c29b7abade2c356b6c29305cbf1
HenriqueMoreira-1 <henriquemoreiradesouza4@gmail.com>
2023-06-27 10:31:40 -0300

    test: should hash user password upon registration

---
+++ src/usecases/register.spec.ts
@@ -0,0 +1,33 @@
+import { expect, it, describe } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+
+describe('Register Use Case', () => {
+  it('Should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    })
+
+    const isPasswordHashed = await compare('123456', user.password_hash)
+
+    expect(isPasswordHashed).toBe(true)
+  })
+})
